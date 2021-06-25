32ec15f98f8f3dfd0dcffe92bdb5ca251febeb37
DivyanshTamrakar <tamrakardivyansh189@gmail.com>
2021-06-26 00:21:23 +0530

    feat : add side navigation drawer

--- FrontEnd/src/Components/Header.js
+++ FrontEnd/src/Components/Header.js
@@ -1,19 +1,22 @@
 import { useWishlist } from "../context/wishlist-context";
 import { Link} from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart ,faShoppingCart,faUser,faStore} from '@fortawesome/free-solid-svg-icons'
-
-
-
+import { faBars,faHeart ,faShoppingCart,faUser,faTimes} from '@fortawesome/free-solid-svg-icons'
+import { useState } from "react";
 
 
 export default function Header(){
+  
   const {WishItemInCart} = useWishlist();
 
-    
+  const [Drawer,setDrawer] = useState(false);
+
     return (
+      <>
       <div className="navigation">
-      <Link to="/"><FontAwesomeIcon icon={faStore}  color="white"  size="2x"/></Link>
+      {/* <Link to="/"> */}
+      <FontAwesomeIcon icon={faBars}  color="white" size="2x" onClick={()=>setDrawer(!Drawer)} style={{cursor:'pointer'}}/>
+      {/* </Link> */}
       <div className="icons">
       <span  class="notification">
       <Link to="/wishlists"><FontAwesomeIcon icon={faHeart}  color="white" /></Link>
@@ -28,6 +31,19 @@ export default function Header(){
       </span>
       </div>
       </div>
+
+      {/* // side bar  */}
+
+     
+      <div id={ Drawer ? 'mySidenav-open':'mySidenav-close'} className="sidenav">
+
+      <div onClick={()=>setDrawer(!Drawer)} className="closebtn">&times;</div>
+      <span>About</span>
+      <span>Services</span>
+      <span>Clients</span>
+      <span>Contact</span>
+      </div>
+        </>
     );
   
   }
