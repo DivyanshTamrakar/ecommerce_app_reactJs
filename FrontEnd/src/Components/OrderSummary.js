565e9c9567ddd394cb35fb8f69215d4ce29bc07a
Divyansh Tamrakar <tamrakardivyansh189@gmail.com>
2021-10-01 22:50:18 +0530

    feat : add payment gateway

--- FrontEnd/src/Components/OrderSummary.js
+++ FrontEnd/src/Components/OrderSummary.js
@@ -5,6 +5,20 @@ import { getData, postData } from "../FetchingApi/fetchApi";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
+function loadScript(src) {
+  return new Promise((resolve) => {
+    const script = document.createElement("script");
+    script.src = src;
+    script.onload = () => {
+      resolve(true);
+    };
+    script.onerror = () => {
+      resolve(false);
+    };
+    document.body.appendChild(script);
+  });
+}
+
 function OrderSummary() {
   const [items, setItems] = useState([]);
   let { loader, setloader } = useLoader();
@@ -29,15 +43,15 @@ function OrderSummary() {
     }
   };
 
-  const placeOrderhandler = async () => {
-    try {
-      await getData(`/carts/delete/all/${userId}`);
-    } catch (error) {
-      console.error(error);
-    }
-    alert(" Your order successfull placed !");
-    navigate("/");
-  };
+  // const placeOrderhandler = async () => {
+  //   try {
+  //     await getData(`/carts/delete/all/${userId}`);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  //   alert(" Your order successfull placed !");
+  //   navigate("/");
+  // };
 
   async function Removehandler(id) {
     setloader(true);
@@ -51,6 +65,56 @@ function OrderSummary() {
       console.log("Error in catch ", e);
       setloader(false);
     }
+  }
+
+  async function displayRazorpay() {
+    const res = await loadScript(
+      "https://checkout.razorpay.com/v1/checkout.js"
+    );
+
+    if (!res) {
+      alert("Razorpay SDK failed to load. Are you online?");
+      return;
+    }
+
+
+
+    const data = await postData({"userId":localStorage.getItem('userId'),"amount":"5000" },'/razorpay');
+ 
+
+    // const data = await fetch("http://localhost:5000/razorpay", {
+    //   method: "POST",
+    // }).then((t) => t.json());
+    if(data.success){console.log("data",data);}
+    
+    var options = {
+      key: "rzp_test_x5vD6ApR8W8yaS", // Enter the Key ID generated from the Dashboard
+      currency: data.currency,
+      amount: "5000",
+      name: "Acme Corp",
+      description: "Test Transaction",
+      image: "https://example.com/your_logo",
+
+      order_id: data.id,
+      handler: function (response) {
+        alert(response.razorpay_payment_id);
+        alert(response.razorpay_order_id);
+        alert(response.razorpay_signature);
+      },
+      prefill: {
+        name: "Gaurav Kumar",
+        email: "gaurav.kumar@example.com",
+        contact: "9999999999",
+      },
+      notes: {
+        address: "Razorpay Corporate Office",
+      },
+      theme: {
+        color: "#3399cc",
+      },
+    };
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
   }
 
   return (
@@ -101,7 +165,7 @@ function OrderSummary() {
           {items.length !== 0 && (
             <div>
               <h2 className="totalamount">{`Total Cart Value = ${totalprice}`}</h2>
-              <button onClick={placeOrderhandler} className="placeOrder">
+              <button onClick={displayRazorpay} className="placeOrder">
                 Place Your Order
               </button>
             </div>
--- ecommerce-backend/index.js
+++ ecommerce-backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Razorpay = require('razorpay');
 const app = express();
 const PORT = 5000;
 var cors = require('cors');
@@ -8,6 +9,7 @@ var userApi = require('./Api/user');
 var cartApi = require('./Api/cart');
 var wishlistApi = require('./Api/wishlist');
 var addressApi = require('./Api/address');
+
 app.use(cors());
 
 
@@ -26,6 +28,44 @@ app.use('/address',addressApi);
 
 app.get('/',(req,res)=>{
   res.status(200).json({message:"Hello World "})})
+
+
+  const razorpay = new Razorpay({
+    key_id: 'rzp_test_x5vD6ApR8W8yaS',
+    key_secret: 'u8qgWgtlNAuiPDtIy6SvojFM'
+  })
+
+
+
+  app.post('/razorpay', async (req, res) => {
+
+    const details = req.body
+    console.log(details);
+    const payment_capture = 1
+    const amount = 499
+    const currency = 'INR'
+  
+    const options = {
+      amount: amount * 100,
+      currency,
+      payment_capture
+    }
+  
+    try {
+      const response = await razorpay.orders.create(options)
+      console.log(response)
+      res.json({
+        success:true,
+        id: response.id,
+        currency: response.currency,
+        amount: response.amount
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
+
 
 
 app.listen(process.env.PORT || PORT, () => {
