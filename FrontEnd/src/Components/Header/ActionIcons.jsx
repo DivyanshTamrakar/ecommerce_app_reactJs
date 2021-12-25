40fec272ceab7ae7db6277a7a985f33563a8ea87
Divyansh Tamrakar <tamrakardivyansh189@gmail.com>
2021-12-25 14:14:10 +0530

    refactor header components

---
+++ FrontEnd/src/Components/Header/ActionIcons.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Header.css';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { useCart } from '../../context/cart-context';
+import { useWishlist } from '../../context/wishlist-context';
+import { Link } from "react-router-dom";
+
+
+
+const IconDesign = { color: 'white', fontSize: '2rem', cursor: 'pointer' };
+function ActionIcons() {
+
+    const { WishItemInCart } = useWishlist();
+    const { itemInCart } = useCart()
+
+    return (
+        <div className='Actionbox'>
+            <div className='notification'>
+                <Link to='/wishlists' className='textDecorationNone'>
+                    <FavoriteIcon sx={IconDesign} />
+                    <span className="badge">{WishItemInCart.length}</span>
+                </Link>
+            </div>
+            <div className='notification'>
+                <Link to='/carts' className='textDecorationNone'>
+                    <ShoppingCartIcon sx={IconDesign} />
+                    <span className="badge">{itemInCart.length}</span>
+                </Link>
+            </div>
+            <Link to='/login' className='textDecorationNone'>
+                <AccountCircleIcon sx={IconDesign} />
+
+            </Link>
+
+        </div>
+    )
+}
+
+export default ActionIcons
--- FrontEnd/src/Components/Header/Header.js
+++ FrontEnd/src/Components/Header/Header.js
@@ -1,98 +1,31 @@
-import { useWishlist } from "../../context/wishlist-context";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-
-import { useState } from "react";
-import { useCart } from "../../context/cart-context";
+import './Header.css';
+import MenuIcon from '@mui/icons-material/Menu';
+import ActionIcons from "./ActionIcons";
+import { Link } from 'react-router-dom';
 
 
-const IconDesign = { color: 'white', fontSize: '2rem' };
-const BadgeDesign = { fontWeight: "600", color: "black400",padding:'5px' };
+const Header = () => {
 
-export default function Header() {
-  const { WishItemInCart } = useWishlist();
-  const { itemInCart } = useCart()
 
-  const [Drawer, setDrawer] = useState(false);
+  return <div className="header">
 
-  return (
-    <>
-      <div className="navigation">
-        <FontAwesomeIcon
-          icon={faBars}
-          color="white"
-          size="lg"
-          onClick={() => setDrawer(!Drawer)}
-          style={{ cursor: "pointer", marginTop: "0.5rem" }}
-        />
-        <Link
-          to="/"
-          style={{
-            textDecoration: "none",
-            fontWeight: "800",
-            fontSize: "35px",
-            color: "white",
-          }}
-        >
-          {" "}
-          Ecommerce Store{" "}
-        </Link>
-        <div className="icons">
-          <span className="notification">
-            <Link to="/wishlists">
-              <FavoriteIcon sx={IconDesign} />
-            </Link>
-            <span style={BadgeDesign} className="badge">{WishItemInCart.length}</span>
-          </span>
-          <span className="notification">
-            <Link to="/carts">
-              <ShoppingCartIcon sx={IconDesign} />
-            </Link>
-            <span style={BadgeDesign} className="badge">{itemInCart.length}</span>
-          </span>
-          <span className="notification">
-            <Link to="/login">
-              <AccountCircleIcon sx={IconDesign} />
-            </Link>
-          </span>
-        </div>
-      </div>
+    <div className="menuIcon" >
+      <MenuIcon sx={{ cursor: 'pointer', color: 'white', fontSize: '2rem' }} />
+    </div>
 
-      {/* // side bar  */}
-      <div
-        id={Drawer ? "mySidenav-open" : "mySidenav-close"}
-        className="sidenav"
-      >
-        <div onClick={() => setDrawer(!Drawer)} className="closebtn">
-          &times;
-        </div>
-        <div type="header-profile">
-          <span
-            style={{
-              textDecoration: "none",
-              fontWeight: "800",
-              fontSize: "35px",
-              color: "white",
-            }}
-          >
-            {" "}
-            Ecommerce{" "}
-          </span>
-        </div>
-        <Link to="/login" style={{ textDecoration: "none" }}>
-          <span>Account</span>
-        </Link>
-        <Link to="/orderhistory" style={{ textDecoration: "none" }}>
-          <span>Orders History</span>
-        </Link>
-        <Link to="/address" style={{ textDecoration: "none" }}>
-          <span>Addresses</span>
-        </Link>
-      </div>
-    </>
-  );
+    <div className="title" >
+      <Link to='/' className='textDecorationNone colorWhite'>
+        E-Commerce App
+      </Link>
+    </div>
+
+    <div className="rightCom">
+      <ActionIcons />
+    </div>
+
+
+  </div>;
 }
+
+
+export default Header;
