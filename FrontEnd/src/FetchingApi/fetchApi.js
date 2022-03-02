a533165ab303513f0eba545c44bc75bfecae198a
Divyansh Tamrakar <tamrakardivyansh189@gmail.com>
2022-03-02 17:30:00 +0530

    refactor : code refactor

--- FrontEnd/src/FetchingApi/fetchApi.js
+++ FrontEnd/src/FetchingApi/fetchApi.js
@@ -1,27 +1,25 @@
 import axios from "axios";
 
- export const url = "https://thawing-spire-10182.herokuapp.com"
-// export const url = "http://localhost:5000";
+//  export const url = "https://thawing-spire-10182.herokuapp.com"
+export const url = "http://localhost:5000";
 // userId which comes from local storage
 export const userId = localStorage.getItem("userId");
 export async function getData(...endpoints) {
-  let final_url = `${url}${endpoints}`;
+  const final_url = `${url}${endpoints}`;
   try {
-    let response = await axios.get(final_url);
-    const resultData = response.data;
-    return resultData;
+    const response = await axios.get(final_url);
+    return response.data;
   } catch (e) {
-    console.log("Error in catch ", e);
+    console.error("Error in catch ", e);
   }
 }
 
 export async function postData(body, ...endpoints) {
-  let final_url = `${url}${endpoints}`;
+  const final_url = `${url}${endpoints}`;
   try {
-    let response = await axios.post(final_url, body);
-    const resultData = response.data;
-    return resultData;
+    const response = await axios.post(final_url, body);
+    return response.data;
   } catch (e) {
-    console.log("Error in catch ", e);
+    console.error("Error in catch ", e);
   }
 }
