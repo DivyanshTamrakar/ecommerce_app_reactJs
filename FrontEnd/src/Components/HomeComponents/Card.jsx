9559deb0d64e7b9e785a7f7d2b9a2314c2c6bc29
Divyansh Tamrakar <tamrakardivyansh189@gmail.com>
2022-03-05 17:07:28 +0530

    style :  fixed card below the caraousel

--- FrontEnd/src/Components/HomeComponents/Card.jsx
+++ FrontEnd/src/Components/HomeComponents/Card.jsx
@@ -1,26 +1,28 @@
-import React, { useEffect } from 'react'
-import { useProduct } from '../../context/ProductContext';
+import React, { useEffect } from "react";
+import { useProduct } from "../../context/ProductContext";
 import ProductCard from "./ProductCard";
-import Loader from '../Loader';
-
+import Loader from "../Loader";
+import "./Card.css";
 const Card = () => {
-    const { GetProductData, filteredData } = useProduct();
-    const userId = localStorage.getItem("userId");
-    useEffect(() => {
-        GetProductData();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [userId]);
-    return (
-        <>
-            {filteredData.length !== 0 ?
-                <div className="productbox">
-                    {filteredData.map((item, index) => {
-                        return <ProductCard key={index} item={item} />
-                    })}
-                </div>
-                : <Loader />}
-        </>
-    )
-}
+  const { GetProductData, filteredData } = useProduct();
+  const userId = localStorage.getItem("userId");
+  useEffect(() => {
+    GetProductData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userId]);
+  return (
+    <>
+      {filteredData.length !== 0 ? (
+        <div className="productbox">
+          {filteredData.map((item, index) => {
+            return <ProductCard key={index} item={item} />;
+          })}
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </>
+  );
+};
 
-export default Card
+export default Card;
