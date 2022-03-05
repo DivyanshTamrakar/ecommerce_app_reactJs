15fbab56cb16365db38302911e528d61d07cd495
Divyansh Tamrakar <tamrakardivyansh189@gmail.com>
2022-03-05 16:35:25 +0530

    feat : added caraousel slider

--- FrontEnd/src/Pages/Home.jsx
+++ FrontEnd/src/Pages/Home.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "react-toastify/dist/ReactToastify.css";
 import Toast from "../Components/toast";
 import Card from "../Components/HomeComponents/Card";
+import Caraousel from "../Components/Caraousel";
 
 const Home = () => {
   return (
     <div>
+      <Caraousel/>
       <Card />
       <Toast />
     </div>
